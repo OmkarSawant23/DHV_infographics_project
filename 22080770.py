9c765447f6985554266ab26ddd6bf87e31cb2e9b
OmkarSawant23 <124716114+OmkarSawant23@users.noreply.github.com>
2024-01-10 19:57:22 +0530

    Add files via upload

--- 22080770.py
+++ 22080770.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 10 19:45:01 2024
-
-@author: OMKAR
-"""
-
 #import statements
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns
-
+from matplotlib.gridspec import GridSpec
 
 def file_name(file) :
     
@@ -48,27 +41,81 @@ def bar_plot(selected_entries,grid) :
     plt.ylabel("Frequency")
     plt.xticks(rotation=90)
 
-def pie_plot(top_product) :
-    top_sold = selected_entries['item_name'].value_counts().nlargest(10)
-    # plotting pie chart using matplotlib.
-    sns.pie(top_sold,
-           labels=selected_entries['item_name'], autopct='%1.0f%%')
-
-    # Adding Title for pie chart
-    plt.title('top sold item in year 2021')
-
+def pie_plot(selected_entries,grid) :
+    plt.subplot(grid[0,1])
+    trans = selected_entries['trans_type'].value_counts()
+    top_trans_names = trans.index
+    top_trans_counts = trans.values
+    explode = [0.3, 0, 0]
+    plt.pie(top_trans_counts, labels=top_trans_names, autopct='%1.0f%%', explode = explode)
+    plt.title('Types of Payment Methods used in 2021')
     
-    # displaying plot.
-    plt.show()
+def lineplot_monthly(selected_entries,grid):
+    plt.subplot(grid[0,2])
+    top_sup = selected_entries['item_name'].value_counts().nlargest(10)
+    selected_entries['date_only'] = pd.to_datetime(selected_entries['date_only'], format='%Y-%m-%d')
+    selected_entries_filter = selected_entries.loc[selected_entries["item_name"].isin(top_sup.index) ]
+    selected_entries_filter['month'] = selected_entries_filter['date_only'].dt.month
+    table = pd.pivot_table(selected_entries_filter, values='quantity', index=['item_name'],
+                       columns=['month'], aggfunc="sum")
+    print(table)
+    selected_entries['month'] = selected_entries['date_only'].dt.month
+    monthly_data = selected_entries.groupby('month')['quantity'].sum()
+    print(table.iloc[0,:].values)
+    print(table.columns)
+    sns.lineplot(y=table.iloc[0,:].values,x=table.columns, marker='o', label = table.index[0] )
+    sns.lineplot(y=table.iloc[1,:].values,x=table.columns, marker='o', label = table.index[1] )
+    sns.lineplot(y=table.iloc[2,:].values,x=table.columns, marker='o', label = table.index[2] )
+    sns.lineplot(y=table.iloc[3,:].values,x=table.columns, marker='o', label = table.index[3] )
+    sns.lineplot(y=table.iloc[4,:].values,x=table.columns, marker='o', label = table.index[4] )
+    plt.title('Monthly Trend for Top Products in year 2021')
+    plt.legend()
+    plt.xlabel('Months')
+    plt.ylabel('Number of Products sold')
+    plt.xticks(range(1, 13), labels=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+    #plt.show()
+    
+def bar_plot_one(selected_entries,grid) :
+    plt.subplot(grid[1,2])
+    top_sup = selected_entries['item_name'].value_counts().nlargest(10)
+    sns.barplot(x=top_sup, y=top_sup.index)
+    plt.title("Top 10 Products sold in 2021")
+    plt.xlabel("Count")
+    plt.ylabel("Products")
+    
+def data_info() :
+     plt.subplot(grid[1,0])
+     text = "Key Note\n\n"\
+           "This dashboard is comprehensive study of\n"\
+           "E_commerce trade in india\n"\
+           "about the top selling products and the top suppliers\n"\
+           "which provides us uderstanding of in demand products in India"
+     plt.text(0.1 , 0.1 , text , ha='left' , va='center' , fontsize=12 ,
+             fontstyle='italic' , fontweight='bold')
+     plt.axis('off')
+     plt.subplot(grid[1 , 1])
+     text = "Student Name: Omkar Shashikant Sawant\n" \
+           "Student ID: 22080770"
+     plt.text(0.1 , 0.1 , text , ha='center' , va='center' , fontsize=12,
+             fontstyle='italic' , fontweight='bold')
+     plt.axis('off')
+     fig.suptitle('E-Commerce Data Analysis for Year 2021' , fontsize=25 , fontweight='bold')
     
      
 #Main Function
 merged_df,merged_df_sorted,selected_entries,selected_year = file_name("file")
-#print(merged_df.head)
+print(merged_df.head)
 print(merged_df_sorted["date_only"])
 print(selected_entries)
 merged_df.to_csv('merged_data.csv', index=False)
 merged_df_sorted.to_csv('sorted_data.csv', index = False)
 selected_entries.to_csv(f'selected_entries_{selected_year}.csv', index=False)
-bar_plot("top_supplier")
-pie_plot("top_product")
+sns.set_theme()
+fig = plt.figure(figsize=(20,10),dpi= 300)
+grid = GridSpec(2,3, width_ratios=[1,1,1], height_ratios=[1,1])
+bar_plot(selected_entries,grid)
+pie_plot(selected_entries,grid)
+lineplot_monthly(selected_entries,grid)
+bar_plot_one(selected_entries,grid)
+data_info()
+fig.savefig("22080770.png", dpi=300)
